1409774ac4eb4d37f2b1979cd393f0b3c510fba5
zakaria-messous <bosszaki2001@gmail.com>
2024-05-23 21:08:53 +0100

    Profile

--- FrontEnd/src/pages/ProfilePage/ProfilePage.jsx
+++ FrontEnd/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,4 @@
 import styles from "./ProfilePage.module.css";
-import { useState } from 'react';
 
 //Components
 import Header from "../../components/Header/Header";
@@ -11,8 +10,38 @@ function ProfilePage() {
     <Header />
 
     <div className={styles.container}>
-      Profile Page
-    </div>
+            <div className={styles.card}>
+                <div className={styles.cardBody}>
+                    <div className={styles.profilePicContainer}>
+                        <img 
+                            src="https://bootdey.com/img/Content/avatar/avatar7.png" 
+                            alt="User" 
+                            className={styles.profilePic} 
+                        />
+                    </div>
+                    <div className={styles.userDetails}>
+                        <h2 className={styles.userName}>John Doe</h2>
+                        <p className={styles.userDetail}>Full Name:</p>
+                        <div className={styles.userdata}>
+                        <p className={styles.userDetail}>John Doe</p>
+                        </div>
+                        <p className={styles.userDetail}>Email:</p>
+                        <div className={styles.userdata}>
+                        <p className={styles.userDetail}>johndoe@example.com</p>
+                        </div>
+                        <p className={styles.userDetail}>Phone:</p>
+                        <div className={styles.userdata}>
+                        <p className={styles.userDetail}>+212 1234-7890</p>
+                        </div>
+                        <p className={styles.userDetail}>Mot de passe:</p>
+                        <div className={styles.userdata}>
+                        <p className={styles.userDetail}>••••••••</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
     
     <Footer />
     </>
