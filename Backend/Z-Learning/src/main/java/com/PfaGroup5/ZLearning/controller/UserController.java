d5d69bcb7435ac6f5797405e7ef4b1ab6735055e
Ahmed <mwar662002@gmail.com>
2024-05-23 13:38:03 -0700

    added sessions

--- Backend/Z-Learning/src/main/java/com/PfaGroup5/ZLearning/controller/UserController.java
+++ Backend/Z-Learning/src/main/java/com/PfaGroup5/ZLearning/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.PfaGroup5.ZLearning.controller;
 
+import com.PfaGroup5.ZLearning.model.Course;
 import com.PfaGroup5.ZLearning.model.User;
 import com.PfaGroup5.ZLearning.service.UserSevice;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
-
+import java.util.List;
 
 
 @RestController
@@ -21,27 +18,54 @@ public class UserController {
     public UserController(UserSevice userSevice) {
         this.userSevice = userSevice;
     }
-
-    @PostMapping("/login/{userName}/{password}")
-    public ResponseEntity login(@PathVariable String email, @PathVariable String password) {
-
+    //login and register
+    @PostMapping("/login/{email}/{password}")
+    public ResponseEntity<User> login(@PathVariable String email, @PathVariable String password) {
         try {
-            userSevice.loginValidation(email, password);
-            return ResponseEntity.ok().build();
+            User user = userSevice.loginValidation(email, password);
+            return ResponseEntity.ok().body(user);
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().body(null);
         }
-
     }
-
     @PostMapping("/register")
-    public ResponseEntity register(User user) {
+    public ResponseEntity<User> register(User user) {
         try {
-            userSevice.register(user);
+            User registeredUser = userSevice.register(user);
+            return ResponseEntity.ok().body(registeredUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+
+    //enroll , unenroll  and getCourses for user
+
+    @PostMapping("/{userID}/{courseID}")
+    public ResponseEntity enrollCourse(@PathVariable String userID, @PathVariable String courseID) {
+        try {
+            userSevice.enrollCourse(userID, courseID);
             return ResponseEntity.ok().build();
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().build();
         }
-
     }
+    @DeleteMapping("/{userID}/{courseID}")
+    public ResponseEntity unenrollCourse(@PathVariable String userID, @PathVariable String courseID) {
+        try {
+            userSevice.unenrollCourse(userID, courseID);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    @GetMapping("/{userID}/courses")
+    public ResponseEntity<List<Course>> getCourses(@PathVariable String userID) {
+        try {
+            return ResponseEntity.ok().body(userSevice.getCourses(userID));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
 }
--- Backend/Z-Learning/src/main/java/com/PfaGroup5/ZLearning/service/UserSevice.java
+++ Backend/Z-Learning/src/main/java/com/PfaGroup5/ZLearning/service/UserSevice.java
@@ -1,37 +1,71 @@
 package com.PfaGroup5.ZLearning.service;
 
+import com.PfaGroup5.ZLearning.model.Course;
 import com.PfaGroup5.ZLearning.model.User;
+import com.PfaGroup5.ZLearning.repository.CourseRepo;
 import com.PfaGroup5.ZLearning.repository.UserRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserSevice {
 
-
+    final public CourseRepo courseRepo;
     final public UserRepo userRepo;
 
-    public UserSevice(UserRepo userRepo) {
+    public UserSevice(UserRepo userRepo, CourseRepo courseRepo) {
         this.userRepo = userRepo;
+        this.courseRepo = courseRepo;
     }
 
 
-    public void loginValidation(String email, String password) {
+    public User loginValidation(String email, String password) {
         User user = userRepo.findByEmail(email);
-
-        if(user == null && !user.getPassword().equals(password)) {
-           throw new RuntimeException("Invalid email or password");
+        if (user == null || !user.getPassword().equals(password)) {
+            throw new RuntimeException("Invalid email or password");
         }
+        return user;
     }
 
-    public void register(User user) {
+    public User register(User user) {
         List<User> users = userRepo.findAll();
         for (User u : users) {
             if (u.getEmail().equals(user.getEmail())) {
                 throw new RuntimeException("Email already exists");
             }
         }
+        return userRepo.insert(user);
+    }
+
+    public void enrollCourse(String userID, String courseID) {
+        User user = userRepo.findById(userID).orElseThrow(() -> new RuntimeException(
+                String.format("Cannot Find User by Id %s", userID)));
+        Course course = courseRepo.findById(courseID).orElseThrow(() -> new RuntimeException(
+                String.format("Cannot Find Course by Id %s", courseID)));
+        List<Course> courses = new ArrayList<>(user.getCourseList());
+        courses.add(course);
+        user.setCourseList(courses);
         userRepo.save(user);
     }
+
+    public void unenrollCourse(String userID, String courseID) {
+        User user = userRepo.findById(userID).orElseThrow(() -> new RuntimeException(
+                String.format("Cannot Find User by Id %s", userID)));
+        Course course = courseRepo.findById(courseID).orElseThrow(() -> new RuntimeException(
+                String.format("Cannot Find Course by Id %s", courseID)));
+        List<Course> courses = new ArrayList<>(user.getCourseList());
+        courses.remove(course);
+        user.setCourseList(courses);
+        userRepo.save(user);
+    }
+
+    public List<Course> getCourses(String userID) {
+        User user = userRepo.findById(userID).orElseThrow(() -> new RuntimeException(
+                String.format("Cannot Find User by Id %s", userID)));
+
+        return user.getCourseList();
+    }
+
 }
